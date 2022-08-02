d895eeaf194dc91b359342d49165993dfc212475
AntoinePiron <pro.piron.antoine@gmail.com>
2022-08-02 20:03:16 +0200

    test

--- app/components/TC/TcAstusEvents.vue
+++ app/components/TC/TcAstusEvents.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-extra-light">
     <v-container>
-      <v-row justify="center">
+      <v-row class="mb-5" justify="center">
         <v-col cols="12" sm="10" md="9" lg="7">
           <div class="text-center">
             <h2 class="ui-title font-weight-bold">Les évènements</h2>
@@ -14,12 +14,28 @@
           </div>
         </v-col>
       </v-row>
+      <v-carousel v-model="model" class="mb-5">
+        <v-carousel-item v-for="event in events" :key="event">
+          <v-sheet height="110%" tile>
+            <v-row class="fill-height" align="center" justify="center">
+              <div class="text-h2">{{ event }}</div>
+            </v-row>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      model: 0,
+      events: ["WEI", "POT", "Soirée retrouvailles", "WEC"]
+    };
+  }
+};
 </script>
 
 <style></style>
