a0d132a4f034872b6f322e3560e28eae24c7c8f3
AntoinePiron <pro.piron.antoine@gmail.com>
2022-08-03 15:09:44 +0200

    feat(astuspage): :sparkles: v1 carousel

--- components/TC/TcAstusEvents.vue
+++ components/TC/TcAstusEvents.vue
@@ -14,11 +14,25 @@
           </div>
         </v-col>
       </v-row>
-      <v-carousel v-model="model" class="mb-5" :show-arrows="false">
-        <v-carousel-item v-for="event in events" :key="event">
-          <v-sheet height="110%" tile>
-            <v-row class="fill-height" align="center" justify="center">
-              <div class="text-h2">{{ event }}</div>
+      <v-carousel
+        cycle
+        interval="8000"
+        v-model="model"
+        class="custom-carousel"
+        :show-arrows="false"
+      >
+        <v-carousel-item v-for="(item, i) in events" :key="i">
+          <v-sheet :class="'entire-sheet-' + i" height="110%" tile>
+            <v-row
+              class="fill-height act-content"
+              align="center"
+              justify="center"
+            >
+              <div class="text-bg">
+                <h1 class="text-h1" style="color: black">{{ item.name }}</h1>
+                <br />
+                <p class="text-h4" style="color: black">{{ item.desc }}</p>
+              </div>
             </v-row>
           </v-sheet>
         </v-carousel-item>
@@ -32,10 +46,86 @@ export default {
   data() {
     return {
       model: 0,
-      events: ["WEI", "POT", "Soirée retrouvailles", "WEC"]
+      events: [
+        {
+          name: "WEI",
+          desc:
+            "L'événement de début d'année à ne surtout pas rater. En TC nous avons la chance de le réaliser sur 3 jours (jeudi à samedi). Au programme : des jeux, de la baignade et des moments de convivialités."
+        },
+        {
+          name: "POT",
+          desc:
+            "Organisé par des élèves volontaires de 3e année le pot a pour but de présenter le département aux élèves du FIMI. L'occasion de se retrouver pour discuter orientation autour d'un cocktail préparé par vos soins ! (Souvent ceux qui font le pot finissent à l'ASTUS d'ailleurs)"
+        },
+        {
+          name: "Soirée retrouvailles",
+          desc:
+            "C'est le premier événement du nouveau mandat fraîchement élu en début d'année (et oui l'ASTUS fonctionne en année civile !). On privatise un bar et on se retrouve tous ensemble pour accueillir chaleureusement tous nos joyeux TC qui étaient partis au S1 en échange."
+        },
+        {
+          name: "WEC",
+          desc:
+            "Un week-end en fin d'année, du canoë, un super camping : quoi de mieux ? C'est le dernier évènement de l'année scolaire et ça permet de vivre un dernier moment tous ensemble absolument fou !"
+        }
+      ]
     };
   }
 };
 </script>
 
-<style></style>
+<style scoped>
+.custom-carousel {
+  margin-bottom: 3%;
+  box-shadow: 5px 5px 5px;
+}
+.act-content {
+  margin-inline: 10%;
+}
+.entire-sheet-0 {
+  background-image: url("@/assets/images/carousel/WEI.jpg");
+  background-size: 100% auto;
+  background-position-y: 50%;
+}
+.entire-sheet-1 {
+  background-image: url("@/assets/images/carousel/pot.jpg");
+  background-size: 100% auto;
+  background-position-y: 75%;
+}
+.entire-sheet-2 {
+  background-image: url("@/assets/images/carousel/retrouvaille.jpg");
+  background-size: 100% auto;
+  background-position-y: 50%;
+}
+.entire-sheet-3 {
+  background-image: url("@/assets/images/carousel/canoe.jpg");
+  background-size: 100% auto;
+  background-position-y: 50%;
+}
+.text-bg {
+  background-color: rgba(255, 255, 255, 0.7);
+  padding: 20px;
+  border-radius: 4px;
+}
+@media (max-width: 991px) {
+  .entire-sheet-0 {
+    background-image: url("@/assets/images/carousel/WEI.jpg");
+    background-size: auto 100%;
+    background-position-x: 50%;
+  }
+  .entire-sheet-1 {
+    background-image: url("@/assets/images/carousel/pot.jpg");
+    background-size: auto 100%;
+    background-position-x: 50%;
+  }
+  .entire-sheet-2 {
+    background-image: url("@/assets/images/carousel/retrouvaille.jpg");
+    background-size: auto 100%;
+    background-position-x: 50%;
+  }
+  .entire-sheet-3 {
+    background-image: url("@/assets/images/carousel/canoe.jpg");
+    background-size: auto 100%;
+    background-position-x: 50%;
+  }
+}
+</style>
