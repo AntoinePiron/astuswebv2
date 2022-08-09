c704f6ad9248ac8b90657058218aa87586bb75e2
AntoinePiron <pro.piron.antoine@gmail.com>
2022-08-09 19:11:44 +0100

    feat(integegration page):

--- pages/integration.vue
+++ pages/integration.vue
@@ -4,7 +4,7 @@
     <TcBlog />
 
     <div style="margin-inline: 1vw" class="bg-tc">
-      <v-row class="mt-13" justify="center">
+      <v-row class="mt-13 custom-space" justify="center">
         <v-col v-for="(item, i) in items" :key="i" cols="12" md="6" lg="6">
           <div class="position-relative mb-15">
             <component :is="item.name"></component>
@@ -49,4 +49,14 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.custom-space {
+  margin-inline: 1vw !important;
+}
+
+@media (max-width: 991px) {
+  .custom-space {
+    margin-inline: 0.5vw !important;
+  }
+}
+</style>
